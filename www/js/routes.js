cecbdc21496d7eb2a4781b4e5b8e5723086ccda9
Apizaco <jesus.rodrigu20@anahuac.mx>
2018-08-21 01:30:30 -0500

    ola

---
+++ www/js/routes.js
@@ -0,0 +1,32 @@
+var routes = [
+    {
+      path: '/register/',
+      url: 'views/register.html',
+    },{
+      path: '/ola/',
+      url: 'views/ola.html',
+    }, 
+    {
+      path: '/despacho/',
+      url: 'views/contact.html',
+    },
+    {
+      path: '/bache/',
+      url: 'views/bache.html',
+    },
+    {
+      path: '/seguridad/',
+      url: 'views/seguridad.html',
+    },{
+       path: '/home/',
+      url: 'views/home.html',
+    },
+    {
+      path: '/login/',
+      url: 'index.html',
+    },
+    {
+       path: '/home/',
+      url: 'views/home.html',
+    }
+  ];
